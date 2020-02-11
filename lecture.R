0ebff65efdc0997f771a59b8ec36f666c6ae528f
David Mason <masond@ufl.edu>
2020-02-11 11:23:42 -0500

    adding information about rasters and vectors

--- lecture.R
+++ lecture.R
@@ -1,1 +1,18 @@
 # 11 February 2020
+# Raster data ####
+	# conisder spatial extent (farthest location in cardinal directions is part
+		# of your raster). 
+	# resolution (area on the ground that each pixel of the raster covers)
+	# data format = .tif, netCDF-CF (large files such as NOAA)
+		# tif has metadata and tags (extent, CRS, values for missing data)
+	# multi-band images can be plotted individually or concurrently
+		# can include time series or multi (+4), hyperspectral (10-15)
+	# Key points
+		# pixelated, each associated with values at a specific location
+		# has extent and resolution
+# Vector data ####
+	# points and lines = shapes 
+	# extent, object type, and CRS
+	# only contains either points, lines, or polygons
+	# represent specific features with attributes
+# Coordinate reference systems ####
