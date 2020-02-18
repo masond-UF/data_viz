9b08dbfebe7c272ab8c79c8d0d600035507a5e89
David Mason <masond@ufl.edu>
2020-02-18 13:25:21 -0500

    just wasted more time

--- Assignment_map_template.R
+++ Assignment_map_template.R
@@ -14,6 +14,8 @@ OSBS_sites <- NEON_sites %>%
   filter(plotID == "OSBS_028" | plotID == "OSBS_030" | plotID == "OSBS_035" |
   			 	plotID == "OSBS_037" | plotID == "OSBS_038" | plotID == "OSBS_041")
 
+
+
 # Make a plot to see what it looks like
 ggplot()+
 	geom_sf(data = OSBS_sites, size = 2, color = "black", fill = "green")+
@@ -34,6 +36,8 @@ ggplot(data = OSBS_EVI_df, aes(x = x, y = y,
 # Make categories
 OSBS_EVI_df_cat <- OSBS_EVI_df %>% 
 	mutate(EVI_cat = cut(OSBS_EVI_df$NEON_D03_OSBS_DP3_404000_3284000_EVI, breaks = 5))
+# OSBS_EVI_df_cat <-  st_as_sf(OSBS_EVI_df_cat, coords = c("x", "y"), 
+														 c# rs = st_crs(OSBS_EVI))
 ################# Read in and convert the structure data ##############
 # Read in the vegetation structure data
 veg_struct <- read.csv("Assignment_files/Observations/OSBS_structure.csv")
@@ -46,7 +50,7 @@ veg_struct_filt <- veg_struct %>%
 
 # Turn the data frame into a spatial data frame using st_as_sf(), and using the raster CRS
 veg_struct_spat <- st_as_sf(veg_struct_filt, coords = c("easting", "northing"),
-														crs = st_crs(OSBS_EVI))
+														crs = st_crs(OSBS_EVI_df_cat))
 
 
 # Make a plot to see what it looks like
@@ -60,18 +64,25 @@ ggplot()+
 
 # Tip: to convert the shapefile CRS into that of the raster and vegetation structure
 # shapefile, use st_transform()
+OSBS_sites_tf <- st_transform(OSBS_sites, crs = st_crs(OSBS_EVI))
 
-
-
+crs(OSBS_sites_tf)
+crs(OSBS_EVI_df_cat)
 
 ################### Plot all information together (raster + vector) ########################### 
 # Plot
 
+ggplot()+
+	geom_raster(data = OSBS_EVI_df_cat, aes(x = x, y = y, fill = EVI_cat))+
+	scale_color_viridis_d()+
+	geom_sf(data = OSBS_sites_tf)+
+	# geom_sf(data = veg_struct_spat) # I can't get this to add 
+	coord_sf()
 
-
+	 
 
 # Save the plot
 
-
+ggsave
 
 
