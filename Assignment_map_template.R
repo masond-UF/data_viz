5335948ff8977e7837901eb2effb60d25dab5be1
David Mason <masond@ufl.edu>
2020-02-19 12:38:33 -0500

    finished assignment

--- Assignment_map_template.R
+++ Assignment_map_template.R
@@ -11,33 +11,46 @@ NEON_sites <- st_read("Assignment_files/Shapefiles/All_Neon_TOS_Polygons_V5/All_
 # Select only the locations we are interested in: OSBS, 2017, specifically plots OSBS_028, OSBS_030, OSBS_035, 
 # OSBS_037, OSBS_038 and OSBS_041.
 OSBS_sites <- NEON_sites %>%
-  filter(plotID == "OSBS_028" | plotID == "OSBS_030" | plotID == "OSBS_035" |
-  			 	plotID == "OSBS_037" | plotID == "OSBS_038" | plotID == "OSBS_041")
-
-
+	filter(plotID %in% c("OSBS_028","OSBS_030", "OSBS_035", "OSBS_037",
+											 "OSBS_038", "OSBS_041"))
 
 # Make a plot to see what it looks like
 ggplot()+
-	geom_sf(data = OSBS_sites, size = 2, color = "black", fill = "green")+
-	ggtitle("Ordway-Swisher Biological Station")+
-	theme_classic()+
-	coord_sf()
+	geom_sf(data = OSBS_sites)
+
 
 
 ############### Read in the raster EVI ################
 # Read in the file
-OSBS_EVI <- raster("Assignment_files/Raster_EVI/NEON_D03_OSBS_DP3_404000_3284000_EVI.tif")
+EVI_rast <- raster("Assignment_files/Raster_EVI/NEON_D03_OSBS_DP3_403000_3284000_EVI.tif")
 # Make it a data frame
-OSBS_EVI_df <- as.data.frame(OSBS_EVI, xy = TRUE)
+EVI_df <- as.data.frame(EVI_rast, xy = TRUE)
 # Plot to see what it looks like
-ggplot(data = OSBS_EVI_df, aes(x = x, y = y, 
+ggplot(data = EVI_df, aes(x = x, y = y, 
 	fill = NEON_D03_OSBS_DP3_404000_3284000_EVI)) + geom_raster()+ 
 	scale_fill_viridis_c() + coord_quickmap()
 # Make categories
-OSBS_EVI_df_cat <- OSBS_EVI_df %>% 
-	mutate(EVI_cat = cut(OSBS_EVI_df$NEON_D03_OSBS_DP3_404000_3284000_EVI, breaks = 5))
-# OSBS_EVI_df_cat <-  st_as_sf(OSBS_EVI_df_cat, coords = c("x", "y"), 
-														 c# rs = st_crs(OSBS_EVI))
+EVI_df_cat <- EVI_df %>% 
+	mutate(EVI_cat = cut(EVI_df$NEON_D03_OSBS_DP3_403000_3284000_EVI, breaks = 3))
+
+EVI_df_cat$EVI_cat <- recode(EVI_df_cat$EVI_cat, "(-0.0407,0.288]" = "Low", 
+										 "(0.288,0.616]" = "Medium",
+										 "(0.616,0.945]" = "High")
+
+OSBS_sites$plotID <- recode(OSBS_sites$plotID, "OSBS_028" = "OSBS 28", 
+														"OSBS_030" = "OSBS 30",
+														"OSBS_035" = "OSBS 35",
+														"OSBS_037" = "OSBS 37",
+														"OSBS_038" = "OSBS 38",
+														"OSBS_041" = "OSBS 41")
+
+EVI_df_cat <- drop_na(EVI_df_cat)
+
+
+ggplot()+
+	geom_bar(data=EVI_df_cat,aes(EVI_cat))
+
+
 ################# Read in and convert the structure data ##############
 # Read in the vegetation structure data
 veg_struct <- read.csv("Assignment_files/Observations/OSBS_structure.csv")
@@ -45,44 +58,53 @@ veg_struct <- read.csv("Assignment_files/Observations/OSBS_structure.csv")
 # Filter for the data needed: 2017, specifically plots OSBS_028, OSBS_030, OSBS_035, 
 # OSBS_037, OSBS_038 and OSBS_041
 veg_struct_filt <- veg_struct %>% 
-	filter(plotID == "OSBS_028" | plotID == "OSBS_030" | plotID == "OSBS_035" |
-				 	plotID == "OSBS_037" | plotID == "OSBS_038" | plotID == "OSBS_041")
+	filter(year == 2017, plotID %in% c("OSBS_028","OSBS_030", "OSBS_035", "OSBS_037",
+											 "OSBS_038", "OSBS_041"))
 
 # Turn the data frame into a spatial data frame using st_as_sf(), and using the raster CRS
-veg_struct_spat <- st_as_sf(veg_struct_filt, coords = c("easting", "northing"),
-														crs = st_crs(OSBS_EVI_df_cat))
+crs_common <- crs(EVI_rast)
+
+veg_struct_sf <- st_as_sf(veg_struct_filt, coords = c("easting", "northing"),
+														crs = crs_common)
 
 
 # Make a plot to see what it looks like
 ggplot()+
-	geom_sf(data = veg_struct_spat, aes(fill = "basal_sum"))+
-	scale_color_viridis_c()+
-	coord_sf()
+	geom_sf(data = veg_struct_sf)
 	
 
 ########### Make sure all CRS are the same ###########
+crs(veg_struct_sf)
+crs(EVI_rast)
+crs(OSBS_sites)
 
 # Tip: to convert the shapefile CRS into that of the raster and vegetation structure
 # shapefile, use st_transform()
-OSBS_sites_tf <- st_transform(OSBS_sites, crs = st_crs(OSBS_EVI))
+OSBS_sites <- st_transform(OSBS_sites, crs_common)
 
-crs(OSBS_sites_tf)
-crs(OSBS_EVI_df_cat)
 
 ################### Plot all information together (raster + vector) ########################### 
 # Plot
 
 ggplot()+
-	geom_raster(data = OSBS_EVI_df_cat, aes(x = x, y = y, fill = EVI_cat))+
-	scale_color_viridis_d()+
-	geom_sf(data = OSBS_sites_tf)+
-	# geom_sf(data = veg_struct_spat) # I can't get this to add 
+	geom_raster(data = EVI_df_cat, aes(x = x, y = y, fill = EVI_cat))+
+	labs(fill='EVI Categories')+
+	scale_fill_viridis_d()+
+	geom_sf(data=OSBS_sites,aes(color=plotID))+
+	labs(color ='Plot')+
+	geom_sf(data=veg_struct_sf)+
+	ggtitle("Ordway—Swisher Biological Station")+
+	xlab("Longitude")+
+	ylab("Latitude")+
+	theme_classic()+
+	theme(plot.title = element_text(hjust = 0.5))+
+	theme(text = element_text(size=15))+
+	theme(axis.text.x = element_text(angle = 90))+
 	coord_sf()
 
-	 
-
 # Save the plot
+ggsave("ordway_EVI.png", plot = ordway_EVI, width = 700, height = 700, 
+			 units = "mm", dpi = 300)
 
-ggsave
 
 
