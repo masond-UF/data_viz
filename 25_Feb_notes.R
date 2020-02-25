4053c2b14bec9bb9e9f8cc1dfe58882765637d12
David Mason <masond@ufl.edu>
2020-02-25 11:56:43 -0500

    class notes before break outs

---
+++ 25_Feb_notes.R
@@ -0,0 +1,12 @@
+# 25 February 2020 ####
+# Define the message ####
+	# compare and contrast
+	# show a pattern
+	# demonstrate process
+	# relationship
+	# changes
+	# variability
+# Reconsider the approach ####
+# Examine the composition ####
+	# left to right, top to bottom
+# Simplify and declutter ####
