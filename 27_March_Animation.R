2b8473c7de000909ae98bb586a225d5f21e9148c
David Mason <masond@ufl.edu>
2020-03-30 22:57:49 -0400

    added the code for cropping per advice from Dr. Johnson and messed around with the limits so the color change was more interesting. can't get the time right.

--- 27_March_Animation.R
+++ 27_March_Animation.R
@@ -10,18 +10,32 @@ library(rgdal)
 library(raster)
 library(tidyverse)
 
+str(thermo)
+
 thermo <- read.csv("animation_data/osbs_fire_loggers_3_25_2020.csv")
 thermo$date_time <-  ymd_hms(thermo$date_time)
+d <- data.frame(Date= format(thermo$date_time, '%d/%m/%Y'), 
+								Time=format(thermo$date_time, '%H:%M:%S'))
+thermo <- cbind(thermo,d)
+thermo$Time <- (hms(thermo$Time))
 osbs <- read_sf("animation_data/os_elev_I2.shp")
-
+osbs.crop <- st_crop(osbs, c(xmin = 403900, 
+														 xmax = 404400, ymin = 3284900, ymax = 3285480))
 
 p1 <- ggplot()+
-	geom_sf(data = osbs_crop)+
+	geom_sf(data = osbs.crop)+
 	geom_point(data = thermo, aes(x = x, y = y, color = temp_C))+
-	scale_color_viridis_c()+
-	transition_time(time = thermo$date_time)+
-	labs(title = "Time {frame_time}")+
+	scale_color_viridis_c(limits = c(27, 40.65))+
+	transition_states(thermo$date_time)+
+	labs(title = "Time:{frame_time}")+
 	theme_bw()
+
+p1
+
+labs(title = "Time:{as.integer(frame_time)}")+
+	
+
+summary(thermo$temp_C)
 
 thermo_sub <- thermo %>% 
 	filter(logger_id %in% c(1, 5, 11, 22))
@@ -41,4 +55,3 @@ p2 <-ggplot(data = thermo_sub, aes(x = date_time, y = temp_C, color = temp_C))+
 		 transition_reveal(date_time)+
 		 facet_wrap(~logger_id, labeller = as_labeller(loggers))
 
-p2
