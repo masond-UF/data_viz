817878738c2d228e31c5ee2151953b611b98b22a
David Mason <masond@ufl.edu>
2020-03-31 12:38:01 -0400

    finished the assignment by adding the functions for saving GIFs

--- 27_March_Animation.R
+++ 27_March_Animation.R
@@ -1,5 +1,5 @@
 # 27 March 2020—Animation assignment
-
+# Install packages and prep data ####
 library(ggplot2)
 library(lubridate)
 library(data.table)
@@ -9,42 +9,42 @@ library(maps)
 library(rgdal)
 library(raster)
 library(tidyverse)
-
-str(thermo)
-
+library(gifski)
+# Read in data
 thermo <- read.csv("animation_data/osbs_fire_loggers_3_25_2020.csv")
+# Convert to time
 thermo$date_time <-  ymd_hms(thermo$date_time)
-d <- data.frame(Date= format(thermo$date_time, '%d/%m/%Y'), 
-								Time=format(thermo$date_time, '%H:%M:%S'))
-thermo <- cbind(thermo,d)
-thermo$Time <- (hms(thermo$Time))
-osbs <- read_sf("animation_data/os_elev_I2.shp")
-osbs.crop <- st_crop(osbs, c(xmin = 403900, 
-														 xmax = 404400, ymin = 3284900, ymax = 3285480))
-
-p1 <- ggplot()+
-	geom_sf(data = osbs.crop)+
-	geom_point(data = thermo, aes(x = x, y = y, color = temp_C))+
-	scale_color_viridis_c(limits = c(27, 40.65))+
-	transition_states(thermo$date_time)+
-	labs(title = "Time:{frame_time}")+
-	theme_bw()
-
-p1
-
-labs(title = "Time:{as.integer(frame_time)}")+
-	
-
+# Find range of temperatures (most are very low temps) for plot 1
 summary(thermo$temp_C)
-
+# Filter out just the loggers we want for plot 2
 thermo_sub <- thermo %>% 
 	filter(logger_id %in% c(1, 5, 11, 22))
-
+# Create labels for the facet wrap in plot 2
 loggers <- c("1" = "Logger 1",
 						 "5" = "Logger 5",
 						 "11" = "Logger 11",
 						 "22" = "Logger 22")
-	
+
+# Bring in the background map
+osbs <- read_sf("animation_data/os_elev_I2.shp")
+# Crop the map
+osbs.crop <- st_crop(osbs, c(xmin = 403900, 
+														 xmax = 404400, ymin = 3284900, ymax = 3285480))
+
+# plot 1 ####
+p1 <- ggplot()+
+	geom_sf(data = osbs.crop)+
+	geom_point(data = thermo, aes(x = x, y = y, color = temp_C))+
+	scale_color_viridis_c(limits = c(27, 40.65))+ # set to limit of 3Q 
+	transition_time(thermo$date_time)+
+	labs(title = "Time:{frame_time}")+
+	theme_bw()
+
+# Save the files
+p1_anim <- gganimate::animate(p1, duration = 60, )
+gganimate::anim_save("p1_anim.gif", animation = p1_anim)
+
+# plot 2 ####
 p2 <-ggplot(data = thermo_sub, aes(x = date_time, y = temp_C, color = temp_C))+
 		 geom_point()+
 	   geom_line()+
@@ -55,3 +55,5 @@ p2 <-ggplot(data = thermo_sub, aes(x = date_time, y = temp_C, color = temp_C))+
 		 transition_reveal(date_time)+
 		 facet_wrap(~logger_id, labeller = as_labeller(loggers))
 
+p2_anim <- gganimate::animate(p2, duration = 30)
+gganimate::anim_save("p2_anim.gif", animation = p2_anim)
